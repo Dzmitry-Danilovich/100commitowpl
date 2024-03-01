660f459797cafd96dda302b670a467fa61a5407f
Dzmitry-Danilovich <153942587+Dzmitry-Danilovich@users.noreply.github.com>
2024-03-01 20:54:18 +0100

    Zmiana

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Test")
+import speech_recognition as sr
+
+recognizer = sr.Recognizer()
+def rozpoznaj_mowe():
+    with sr.Microphone() as source:
+        print("Proszę mówić...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    try:
+        text = recognizer.recognize_google(audio, language="pl-PL")
+        print("Rozpoznano: " + text)
+    except sr.UnknownValueError:
+        print("Nie rozpoznano mowy")
+    except sr.RequestError as e:
+        print("Błąd serwera; {0}".format(e))
+
+
+while True:
+    rozpoznaj_mowe()
