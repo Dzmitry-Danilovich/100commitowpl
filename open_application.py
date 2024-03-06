c9211353c367c481446c049eb8e390eaaf09d5f3
Dzmitry-Danilovich <153942587+Dzmitry-Danilovich@users.noreply.github.com>
2024-03-06 21:33:21 +0100

    Szablon

--- open_application.py
+++ open_application.py
@@ -1,1 +1,10 @@
 #program powienien otwierać aplikacji i foldery
+
+import subprocess
+
+def run_application(application_path, arguments=[]):
+    try:
+        subprocess.run([application_path] + arguments)
+    except Exception as e:
+        print(f"Błąd podczas uruchamiania aplikacji: {e}")
+
