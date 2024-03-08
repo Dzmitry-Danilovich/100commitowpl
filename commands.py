fa47008d7f7b0a224c64a6c2cfd18b6a713769b6
Dzmitry-Danilovich <153942587+Dzmitry-Danilovich@users.noreply.github.com>
2024-03-08 20:12:53 +0100

    minimalny zmiany

--- commands.py
+++ commands.py
@@ -12,7 +12,7 @@ def check(text):
         return webbrowser.open('https://www.youtube.com')
     elif "scrapping" in text:
         scraping.get_html_content( panel_url.enter_url())
-    elif "Open Aplication":
-        return open_application.run_application(application_path, arguments=[])
+    elif "open application" in text:
+        return open_application.run_application()
     else:
         return f"Nie raspoznany tekst: {text}"
