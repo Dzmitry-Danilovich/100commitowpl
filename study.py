4bffea1cdec273dc57eee62bb05fde4013fd6ea0
Dzmitry-Danilovich <153942587+Dzmitry-Danilovich@users.noreply.github.com>
2024-03-20 21:49:57 +0100

    Study

--- study.py
+++ study.py
@@ -6,3 +6,47 @@ def study(array, answer):
     W1_rounded = np.round(W1, 4)
 
     print(array)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [1], [1], [0]])
+
+np.random.seed(1)
+input_neurons = 2
+hidden_neurons = 4
+output_neurons = 1
+learning_rate = 0.1
+
+weights_input_hidden = np.random.uniform(-1, 1, (input_neurons, hidden_neurons))
+weights_hidden_output = np.random.uniform(-1, 1, (hidden_neurons, output_neurons))
+
+for epoch in range(10000):
+
+    hidden_layer_input = np.dot(X, weights_input_hidden)
+    hidden_layer_output = sigmoid(hidden_layer_input)
+    output_layer_input = np.dot(hidden_layer_output, weights_hidden_output)
+    output_layer_output = sigmoid(output_layer_input)
+
+    output_error = y - output_layer_output
+    if epoch % 1000 == 0:
+        print("Error:", np.mean(np.abs(output_error)))
+
+    output_delta = output_error * sigmoid_derivative(output_layer_output)
+    hidden_error = output_delta.dot(weights_hidden_output.T)
+    hidden_delta = hidden_error * sigmoid_derivative(hidden_layer_output)
+
+    weights_hidden_output += hidden_layer_output.T.dot(output_delta) * learning_rate
+    weights_input_hidden += X.T.dot(hidden_delta) * learning_rate
+
+hidden_layer_input = np.dot(X, weights_input_hidden)
+hidden_layer_output = sigmoid(hidden_layer_input)
+output_layer_input = np.dot(hidden_layer_output, weights_hidden_output)
+output_layer_output = sigmoid(output_layer_input)
+
+print("Output after training:")
+print(output_layer_output)
