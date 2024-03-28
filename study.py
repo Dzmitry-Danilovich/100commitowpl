e223a756d4767697de298c83f707c8e9aaf8112e
Dzmitry-Danilovich <153942587+Dzmitry-Danilovich@users.noreply.github.com>
2024-03-28 18:47:18 +0100

    Update study metods

--- study.py
+++ study.py
@@ -1,52 +1,19 @@
 import numpy as np
 
-def study(array, answer):
-
-    W1 = np.random.rand(len(array), len(array[0])) * 2 - 1
-    W1_rounded = np.round(W1, 4)
-
-    print(array)
-
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-def sigmoid_derivative(x):
-    return x * (1 - x)
+def study(array, answer):
 
-X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-y = np.array([[0], [1], [1], [0]])
+    np.set_printoptions(threshold=np.inf)
+    np.set_printoptions(suppress=True)
+    W1 = np.random.uniform(-1, 1, [128, 156816])
+    W2 = np.random.uniform(-1, 1, [10, 128])
 
-np.random.seed(1)
-input_neurons = 2
-hidden_neurons = 4
-output_neurons = 1
-learning_rate = 0.1
+    sum1 = np.dot(W1, array)
+    out1 = np.array([sigmoid(i) for i in sum1])
 
-weights_input_hidden = np.random.uniform(-1, 1, (input_neurons, hidden_neurons))
-weights_hidden_output = np.random.uniform(-1, 1, (hidden_neurons, output_neurons))
+    sum2 = np.dot(W2, out1)
+    out2 = np.array([sigmoid(i) for i in sum2])
 
-for epoch in range(10000):
-
-    hidden_layer_input = np.dot(X, weights_input_hidden)
-    hidden_layer_output = sigmoid(hidden_layer_input)
-    output_layer_input = np.dot(hidden_layer_output, weights_hidden_output)
-    output_layer_output = sigmoid(output_layer_input)
-
-    output_error = y - output_layer_output
-    if epoch % 1000 == 0:
-        print("Error:", np.mean(np.abs(output_error)))
-
-    output_delta = output_error * sigmoid_derivative(output_layer_output)
-    hidden_error = output_delta.dot(weights_hidden_output.T)
-    hidden_delta = hidden_error * sigmoid_derivative(hidden_layer_output)
-
-    weights_hidden_output += hidden_layer_output.T.dot(output_delta) * learning_rate
-    weights_input_hidden += X.T.dot(hidden_delta) * learning_rate
-
-hidden_layer_input = np.dot(X, weights_input_hidden)
-hidden_layer_output = sigmoid(hidden_layer_input)
-output_layer_input = np.dot(hidden_layer_output, weights_hidden_output)
-output_layer_output = sigmoid(output_layer_input)
-
-print("Output after training:")
-print(output_layer_output)
+    print(out2)
